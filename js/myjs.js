c6684d486b3e2a3ac7d554ba5530806ca1ae247d
noelg <goldenchild_gt@yahoo.com>
2017-02-10 08:39:39 -0400

    dynamic objects

--- js/myjs.js
+++ js/myjs.js
@@ -8,6 +8,36 @@
         return "Hello World, Punk!";
     }
 
+//this is an object
+    function getVehicle(theYear, theMake, theModel) {
+
+        var vehicle = new Object();
+        vehicle.model = theModel;//assign property to vehicle object with a parameter that's passed in 
+        vehicle.make = theMake;
+        vehicle.year = theYear;
+        vehicle.getInfo = function () {
+            return 'Vehicle: ' + this.year + ' ' + this.make + ' ' + this.model;
+        };
+
+        return vehicle;
+    }
+
+    var obj = new getVehicle("2000", "Honda", "CRV");
+
+    alert(obj.getInfo());
+
+//this is a class with encapsulation
+    function Vehicle(theYear, theMake, theModel) {
+        var year = theYear; //use var to prevent data from being added to the global namespace
+        var make = theMake;
+        var model = theModel;
+        this.getInfo = function () {  //public so no var; use 'this' +
+            return 'Vehicle: ' + year + ' ' + make + ' ' + model;
+        };
+    }
+
+
+
 
     //function howdy2() {
     //    var btn = document.getElementById('btn1');
